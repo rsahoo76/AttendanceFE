9e3332faccaec83d5247f4d8ae488a5b4571ba6f
RutujaKakad <rutuja-dilip.kakad@capgemini.com>
2024-12-13 12:59:52 +0530

    Added header to teacher component and added username column to attendance table

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 
 @Component({
@@ -9,9 +10,55 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   sidebarVisible: boolean = false;
+  sidebarOpen: boolean = false;
+  user!: any;
+  UserData:  any;
 
-  constructor(private router: Router){}
 
+  constructor(private router: Router,
+    private authservice: AuthService
+  ){}
+
+  ngOnInit(): void {
+  this.authservice.getLoggedUser().subscribe(
+    (data: any) => {
+      this.user = data,
+      this.SetUserData(this.user),
+        (err: any) => console.error(err)
+    },
+    (error: any) => {
+      console.error('Error fetching user data', error);
+    }
+  );
+  }
+
+  SetUserData(user1: any){
+    this.UserData = user1;
+  }
+
+  public toggleSidebar() {
+    if ($('.sidebar').css('left') == "-200px") {
+      this.sidebarOpen = true;
+      $('.sidebar').css('left', 0);
+      $('.sidebar').css('box-shadow', '5px 5px 2px #e6e6e6aa');
+    } else {
+      this.sidebarOpen = false;
+      $('.sidebar').css('left', '-200px');
+      $('.sidebar').css('box-shadow', 'none');
+    }
+  }
+
+  hasRole(){
+
+    let role = null;
+    if (this.UserData) {
+      role = this.UserData.roles.name;
+      if(role == 'teacher' || role == 'admin'){
+        return true;
+      }
+}
+return false;
+}
 
   logout() {
     const confirmLogout = confirm('Are you sure you want to logout?');
